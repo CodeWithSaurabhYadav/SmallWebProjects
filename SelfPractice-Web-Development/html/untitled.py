35339a87ce18da1fc1038de8347a417140db0714
saurabh <50881320+CodeWithSaurabhYadav@users.noreply.github.com>
2022-06-07 04:26:59 +0530

    Add files via upload

---
+++ SelfPractice-Web-Development/html/untitled.py
@@ -0,0 +1,9 @@
+# _init_method
+class EmployeName:
+    def _init_(self,name,salary):
+        self.name = name
+        self.salary = salary
+    def next():
+        print(f"{self.name}")
+        print(f"{self.salary}")
+apply=EmployeName('mohit',100)
